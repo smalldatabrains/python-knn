6e9984944616e5d9e6d623bfdb226331fbffff63
smalldatabrains <smalldatabrains@gmail.com>
2018-03-17 22:19:55 +0100

    data analysis update

--- knn.py
+++ knn.py
@@ -7,18 +7,21 @@
 import numpy as np
 import pandas as pd
 import math
-from sklearn import preprocessing,cross_validation
+import matplotlib.pyplot as plt
+from sklearn import preprocessing,model_selection
+from collections import Counter
+import scipy
 import os
 
 df=pd.read_csv('data/data.csv',sep=";")
-
+print(df)
 X=np.array(df.drop(['left'],1))
-X=preprocessing.normalize(X)
+X_normed=preprocessing.normalize(X)
 y=np.array(df['left'])
 
-X_train,X_test,y_train,y_test=cross_validation.train_test_split(X,y,test_size=0.2)
+X_train,X_test,y_train,y_test=model_selection.train_test_split(X_normed,y,test_size=0.2)
 
-new_individual=np.array([0.10,0.1,10,150,7,0,0])
+new_individual=np.array([0.45,0.47,2,160,3,0,0])
 
 #examples
 ind1=np.array([3,3])
@@ -32,26 +35,65 @@ def distance(ind1,ind2):
 	distance=np.sum((ind1-ind2)**2)
 	return distance
 
+#create the table if distances between the new example and every other labelled data
 def kdistance(new_example,labelled_data):
 	dist=[]
 	for i in list(range(0,len(labelled_data))):
-		print(i)
 		dist.append(distance(new_example,labelled_data[i]))
 	return dist
 
-def knn(kdistance_table):
-	enum=np.argmin(kdistance_table)
-	prediction=X_train[enum]
-	print("The new individual is close to ",prediction)
+def knn(k,kdistance_table):
+	enum=np.argsort(kdistance_table)[:k]
+	predictions=y_train[enum]
+	c=Counter(predictions)
+	key=[element for element, count in c.most_common(1)]
+	return key[0]
 
-def vote():
-	pass
+def accuracy(X_test,y_test,X_train):
+	total=len(y_test)
+	liste=[]
+	for example in range(len(X_test)):
+		prediction=knn(5,kdistance(X_test[example],X_train))
+		liste.append(prediction)
+	good_predictions=np.sum(liste==y_test)
+	accuracy=good_predictions/total
+	return accuracy
 
-#Calcul of distances
-kdistance_table=kdistance(new_individual,X_train)
 
-#Knn
-knn(kdistance_table)
+# #Calcul of distances
+# kdistance_table=kdistance(new_individual,X_train)
+
+# #Knn
+# knn(5,kdistance_table)
+
+#Confusion matrix
+# print("accuracy of algorithm is",accuracy(X_test,y_test,X_train))
+
+
+#data analysis
+analysis=True
+if analysis==True:
+	stats=scipy.stats.describe(X)
+	print(stats.nobs)
+	print(stats.minmax)
+	print(stats.mean)
+	print(stats.variance)
+	print(stats.skewness)
+	time_spend_hist=plt.hist(df['time_spend_company'])
+	plt.title('time_spend_company')
+	plt.show()
+	montly_hours_hist=plt.hist(df['average_montly_hours'])
+	plt.title('average_montly_hours')
+	plt.show()
+	number_project_hist=plt.hist(df['number_project'])
+	plt.title('number_project')
+	plt.show()
+	number_project_hist=plt.hist(df['promotion_last_5years'])
+	plt.title('promotion_last_5years')
+	plt.show()
+	number_project_hist=plt.hist(df['left'])
+	plt.title('skewness of the data')
+	plt.show()
 
 
 
--- knnsci.py
+++ knnsci.py
@@ -14,37 +14,41 @@ y=np.array(df['left'])
 
 X_train,X_test,y_train,y_test=model_selection.train_test_split(X,y,test_size=0.2)
 
-clf=neighbors.KNeighborsClassifier()
+clf=neighbors.KNeighborsClassifier() #k default value is 5
 clf.fit(X_train,y_train)
 
 accuracy=clf.score(X_test,y_test)
 print(accuracy)
 
 
+
+
 new_example=np.array([0.10,0.1,10,150,7,0,0])
 new_example=new_example.reshape(1,-1)
 prediction=clf.predict(new_example)
+
+
 if (prediction==1):
 	p="leave the company"
 else:
-	p="stay int the company"
+	p="stay in the company"
 print("For individual with characteristics",new_example,"our prediction is that he will", p)
 
-#tsne with sklearn
-X=pd.DataFrame(X)
-tsne=TSNE(n_components=3,learning_rate=10)
-tsne_results=tsne.fit_transform(X)
+# #tsne with sklearn
+# X=pd.DataFrame(X)
+# tsne=TSNE(n_components=3,learning_rate=10)
+# tsne_results=tsne.fit_transform(X)
 
-df_tsne=X.copy()
-df_tsne['x-tsne']=tsne_results[:,0]
-df_tsne['y-tsne']=tsne_results[:,1]
-df_tsne['z-tsne']=tsne_results[:,2]
-df_tsne['label']=y
+# df_tsne=X.copy()
+# df_tsne['x-tsne']=tsne_results[:,0]
+# df_tsne['y-tsne']=tsne_results[:,1]
+# df_tsne['z-tsne']=tsne_results[:,2]
+# df_tsne['label']=y
 
-# chart=ggplot(df_tsne,aes(x='x-tsne',y='y-tsne',color='label'))+geom_point(size=70,alpha=0.1)+ggtitle("TSNE dimensions colored by group")
-# print(chart)
+# # chart=ggplot(df_tsne,aes(x='x-tsne',y='y-tsne',color='label'))+geom_point(size=70,alpha=0.1)+ggtitle("TSNE dimensions colored by group")
+# # print(chart)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(df_tsne['x-tsne'],df_tsne['y-tsne'],df_tsne['z-tsne'],c=df_tsne['label'])
-plt.show()
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# ax.scatter(df_tsne['x-tsne'],df_tsne['y-tsne'],df_tsne['z-tsne'],c=df_tsne['label'])
+# plt.show()
